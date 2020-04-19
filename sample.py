fa73100e2a0915b6bf4b16d7874c3d1d0d8e14b5
Aadit Agarwal <43968238+aaditagarwal@users.noreply.github.com>
2020-04-19 10:11:13 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,9 @@
+def sample_function(process):
+	#algorithm here
+	execution_order = [process[0][2],process[1][2]]
+	return execution_order
+
+process = []
+process.append([1,5,0])
+process.append([2,4,0])
+sample_function(process)
