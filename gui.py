df061e1fa067b43a6cec493d22e7c638ae5cae65
madhavik0512 <madhavik0512@gmail.com>
2020-04-23 00:00:25 +0530

    design 4.3

--- gui.py
+++ gui.py
@@ -199,26 +199,26 @@ def goto_user_queue():
     lab1 = tk.Label(second, text="Process ID:", font=("New Times Roman", 20, "normal"))
     lab2 = tk.Label(second, text="Burst Time:", font=("New Times Roman", 20, "normal"))
     lab3 = tk.Label(second, text="Arrival Time:", font=("New Times Roman", 20, "normal"))
-    lab4 = tk.Label(second, text="Process ID", font=("New Times Roman", 10, "normal"))
-    lab5 = tk.Label(second, text="Burst Time", font=("New Times Roman", 10, "normal"))
-    lab6 = tk.Label(second, text="Arrival Time", font=("New Times Roman", 10, "normal"))
+
     e1.grid(row=1, column=0, padx=115, pady=10, ipady=4, ipadx=2)
     e2.grid(row=1, column=1, padx=115, pady=10, ipady=4, ipadx=2)
     e3.grid(row=1, column=2, padx=115, pady=10, ipady=4, ipadx=2)
     lab1.grid(row=0, column=0, padx=30, pady=30)
     lab2.grid(row=0, column=1, padx=20, pady=30)
     lab3.grid(row=0, column=2, padx=30, pady=30)
-    lab4.grid(row=3, column=0, padx=30, pady=20)
-    lab5.grid(row=3, column=1, padx=20, pady=20)
-    lab6.grid(row=3, column=2, padx=30, pady=20)
+
     global row
     row = 30
+    global count
+    count = 0
     queue = []
     def give_row():
         global row
         row += 1
         return row
     def add_process():
+        global count
+        count += 1
         try:
             pid = int(e1.get())
             burst_time = int(e2.get())
@@ -236,6 +236,13 @@ def goto_user_queue():
             messagebox.showwarning("Max Inputs Reached!", "User can input maximum of 7 processes.")
             return
         row1 = give_row()
+        if count == 1:
+            lab4 = tk.Label(second, text="Process ID", font=("New Times Roman", 10, "normal"))
+            lab5 = tk.Label(second, text="Burst Time", font=("New Times Roman", 10, "normal"))
+            lab6 = tk.Label(second, text="Arrival Time", font=("New Times Roman", 10, "normal"))
+            lab4.grid(row=3, column=0, padx=30, pady=10)
+            lab5.grid(row=3, column=1, padx=20, pady=10)
+            lab6.grid(row=3, column=2, padx=30, pady=10)
         value1 = tk.Label(second, text = pid, font=("Times New Roman", 15, "normal")).grid(row=row1, column=0,)
         value2 = tk.Label(second, text=burst_time, font=("Times New Roman", 15, "normal")).grid(row=row1, column=1)
         value3 = tk.Label(second, text=arr_time, font=("Times New Roman", 15, "normal")).grid(row=row1, column=2)
@@ -243,10 +250,10 @@ def goto_user_queue():
         e1.delete(0, tk.END)
         e2.delete(0, tk.END)
         e3.delete(0, tk.END)
+    count=0
     b1 = tk.Button(second, text="Go to Main", height=2, command=lambda:goto_main(second))
-    b2 = tk.Button(second, text="Add Process",height=2,  command=add_process)
-    b3 = tk.Button(second, text="Submit",height=2, command=lambda:goto_submission(second, queue))
-
+    b2 = tk.Button(second, text="Add Process", height=2,  command=add_process)
+    b3 = tk.Button(second, text="Submit", height=2, command=lambda:goto_submission(second, queue))
     b1.grid(row=2, column=0, padx=50, pady=50, sticky=tk.NSEW)
     b2.grid(row=2, column=1, padx=50, pady=50, sticky=tk.NSEW)
     b3.grid(row=2, column=2, padx=50, pady=50, sticky=tk.NSEW)
